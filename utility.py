8ce2e4775ddfb3be99a66aaa729b6492152e7b56
zolfaShefreie <z.shefreie@gmail.com>
2021-06-14 02:21:20 +0430

    add a file + add two func

--- utility.py
+++ utility.py
@@ -1,5 +1,9 @@
 import os
 import pandas as pd
+import parsivar
+import codecs
+# from sets import Set
+from hazm import Normalizer
 
 TRAIN_PATH = './Dataset/Train'
 TEST_PATH = './Dataset/Test'
@@ -16,11 +20,42 @@ def get_df(path):
                 file_path = full_path + '/' + entry
                 if not os.path.isdir(file_path):
                     file = open(file_path, encoding='utf8')
-                    list_row.append({'category': each, 'text': file.read()})
+                    text = preprocess_text(file.read())
+                    print('hehe')
+                    list_row.append({'category': each, 'text': text})
                     file.close()
     return pd.DataFrame(list_row)
 
 
 def preprocess_text(text):
-    pass
+    text = parsivar.Normalizer().normalize(text)
+    tokenizer = parsivar.Tokenizer()
+    stop_words = get_stop_words()
+    tokens = tokenizer.tokenize_words(text)
+    new_text = str()
+    for each in tokens:
+        new_word = parsivar.FindStems().convert_to_stem(each)
+        if '#' in new_word:
+            new_word = new_word.split('#')[0]
+        if new_word in stop_words:
+            continue
+        new_text += ' ' + new_word
+    return new_text
 
+
+def get_stop_words():
+    nmz = Normalizer()
+    stops = "\n".join(
+        sorted(
+            list(
+                set(
+                    [
+                        nmz.normalize(w) for w in codecs.open('./persian', encoding='utf-8').read().split('\n') if w
+                    ]
+                )
+            )
+        )
+    )
+    return stops
+
+print(get_df(TRAIN_PATH))
